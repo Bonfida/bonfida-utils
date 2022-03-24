3036bb1993be47139e3fb2e4e519f12e35856c5e
Lcchy <lcchy@mailbox.org>
2022-03-24 19:02:51 +0100

    Add the python autobindings tool

--- autobindings/src/lib.rs
+++ autobindings/src/lib.rs
@@ -1,27 +1,39 @@
+use convert_case::{Case, Casing};
+use proc_macro2::TokenTree;
+use std::time::Instant;
 use std::{
     collections::HashMap,
     fs::File,
     io::{Read, Write},
 };
-
-use convert_case::{Case, Casing};
-use proc_macro2::TokenTree;
 use syn::{
-    punctuated::Punctuated, token::Comma, AngleBracketedGenericArguments, Attribute, Expr, ExprLit,
-    Field, Fields, FieldsNamed, GenericArgument, Item, ItemEnum, ItemStruct, Lit, Path,
-    PathArguments, PathSegment, Type, TypeArray, TypePath, TypeReference, Variant,
+    punctuated::Punctuated, token::Comma, Attribute, Expr, ExprLit, Field, Fields, FieldsNamed,
+    Item, ItemEnum, ItemStruct, Lit, Path, Type, TypeArray, TypePath, TypeReference, Variant,
 };
 
-use std::time::Instant;
+use crate::js_generate::js_process_file;
+use crate::py_generate::py_process_file;
 
-const HEADER: &str = include_str!("templates/template.ts");
+pub mod js_generate;
+pub mod py_generate;
 
-pub fn generate(instructions_path: &str, instructions_enum_path: &str, output_path: &str) {
+#[derive(Debug, Clone, Copy)]
+pub enum TargetLang {
+    Javascript,
+    Python,
+}
+
+pub fn generate(
+    instructions_path: &str,
+    instructions_enum_path: &str,
+    target_lang: TargetLang,
+    output_path: &str,
+) {
     let now = Instant::now();
     let path = std::path::Path::new(instructions_path);
     let (instruction_tags, use_casting) = parse_instructions_enum(instructions_enum_path);
     let directory = std::fs::read_dir(path).unwrap();
-    let mut output = get_header();
+    let mut output = get_header(target_lang);
     for d in directory {
         let file = d.unwrap();
         let module_name = std::path::Path::new(&file.file_name())
@@ -33,12 +45,20 @@ pub fn generate(instructions_path: &str, instructions_enum_path: &str, output_pa
         let instruction_tag = instruction_tags
             .get(&module_name)
             .unwrap_or_else(|| panic!("Instruction not found for {}", module_name));
-        let s = process_file(
-            &module_name,
-            *instruction_tag,
-            file.path().to_str().unwrap(),
-            use_casting,
-        );
+        let s = match target_lang {
+            TargetLang::Javascript => js_process_file(
+                &module_name,
+                *instruction_tag,
+                file.path().to_str().unwrap(),
+                use_casting,
+            ),
+            TargetLang::Python => py_process_file(
+                &module_name,
+                *instruction_tag,
+                file.path().to_str().unwrap(),
+                use_casting,
+            ),
+        };
         output.push_str(&s);
     }
 
@@ -74,215 +94,10 @@ pub fn parse_instructions_enum(instructions_enum_path: &str) -> (HashMap<String,
     (result_map, use_casting)
 }
 
-pub fn get_header() -> String {
-    HEADER.to_owned()
-}
-
-pub fn process_file(
-    module_name: &str,
-    instruction_tag: usize,
-    path: &str,
-    use_casting: bool,
-) -> String {
-    let mut f = File::open(path).unwrap();
-    let mut raw_string = String::new();
-    f.read_to_string(&mut raw_string).unwrap();
-
-    let ast: syn::File = syn::parse_str(&raw_string).unwrap();
-    let accounts_struct_item = find_struct("Accounts", &ast);
-    let params_struct_item = find_struct("Params", &ast);
-
-    let params_fields = get_struct_fields(params_struct_item);
-    let accounts_fields = get_struct_fields(accounts_struct_item);
-    let mut statements = vec![
-        format!("export class {}Instruction {{", snake_to_camel(module_name)),
-        if use_casting {
-            "tag: BN;"
-        } else {
-            "tag: number;"
-        }
-        .to_owned(),
-    ];
-    let mut declaration_statements = vec![];
-    let mut schema_statements = vec![if use_casting {
-        "[\"tag\", \"u64\"],"
-    } else {
-        "[\"tag\", \"u8\"],"
-    }
-    .to_owned()];
-    let mut accounts_statements = vec!["programId: PublicKey,".to_owned()];
-    let mut keys_statements = vec![];
-
-    let mut assign_statements = vec![if use_casting {
-        format!("this.tag = new BN({});", instruction_tag)
-    } else {
-        format!("this.tag = {};", instruction_tag)
-    }];
-    for Field {
-        attrs: _,
-        vis: _,
-        ident,
-        colon_token: _,
-        ty,
-    } in params_fields
-    {
-        let camel_case_ident = snake_to_camel(&ident.as_ref().unwrap().to_string());
-        declaration_statements.push(format!("{}: {};", camel_case_ident, type_to_js(&ty)));
-        schema_statements.push(format!(
-            "[\"{}\", {}],",
-            camel_case_ident,
-            type_to_borsh(&ty)
-        ));
-        if camel_case_ident == "padding" {
-            assign_statements.push(format!(
-                "this.padding = (new Uint8Array({})).fill(0)",
-                padding_len(&ty)
-            ));
-        } else {
-            assign_statements.push(type_assignment(&ty, &camel_case_ident));
-        }
-    }
-    for Field {
-        attrs,
-        vis: _,
-        ident,
-        colon_token: _,
-        ty,
-    } in accounts_fields
-    {
-        let (writable, signer) = get_constraints(&attrs);
-        let camel_case_ident = snake_to_camel(&ident.as_ref().unwrap().to_string());
-        if is_slice(&ty) {
-            accounts_statements.push(format!("{}: PublicKey[],", camel_case_ident));
-            keys_statements.push(format!("for (let k of {}) {{", camel_case_ident));
-            keys_statements.push("keys.push({".to_owned());
-            keys_statements.push("pubkey: k,".to_owned());
-            keys_statements.push(format!("isSigner: {},", signer));
-            keys_statements.push(format!("isWritable: {},", writable));
-            keys_statements.push("});".to_owned());
-            keys_statements.push("}".to_owned());
-        } else if is_option(&ty) {
-            accounts_statements.push(format!("{}?: PublicKey,", camel_case_ident));
-            keys_statements.push(format!("if (!!{}) {{", camel_case_ident));
-            keys_statements.push("keys.push({".to_owned());
-            keys_statements.push(format!("pubkey: {},", camel_case_ident));
-            keys_statements.push(format!("isSigner: {},", signer));
-            keys_statements.push(format!("isWritable: {},", writable));
-            keys_statements.push("});".to_owned());
-            keys_statements.push("}".to_owned());
-        } else {
-            accounts_statements.push(format!("{}: PublicKey,", camel_case_ident));
-            keys_statements.push("keys.push({".to_owned());
-            keys_statements.push(format!("pubkey: {},", camel_case_ident));
-            keys_statements.push(format!("isSigner: {},", signer));
-            keys_statements.push(format!("isWritable: {},", writable));
-            keys_statements.push("});".to_owned());
-        }
-    }
-    statements.extend(declaration_statements.clone());
-    statements.push("static schema: Schema = new Map([".to_owned());
-    statements.push("[".to_owned());
-    statements.push(format!("{}Instruction,", snake_to_camel(module_name)));
-    statements.push("{".to_owned());
-    statements.push("kind: \"struct\",".to_owned());
-    statements.push("fields: [".to_owned());
-    statements.extend(schema_statements.into_iter());
-    statements.push("],".to_owned());
-    statements.push("},".to_owned());
-    statements.push("],".to_owned());
-    statements.push("]);".to_owned());
-    if declaration_statements.is_empty() {
-        statements.push("constructor() {".to_owned());
-    } else {
-        statements.push("constructor(obj: {".to_owned());
-        statements.extend({
-            declaration_statements.retain(|e| !e.contains("padding"));
-            declaration_statements
-        });
-        statements.push("}) {".to_owned());
-    }
-    statements.extend(assign_statements);
-    statements.push("}".to_owned());
-
-    statements.push("serialize(): Uint8Array {".to_owned());
-    statements.push(format!(
-        "return serialize({}Instruction.schema, this);",
-        snake_to_camel(module_name)
-    ));
-    statements.push("}".to_owned());
-    statements.push("getInstruction(".to_owned());
-    statements.extend(accounts_statements);
-    statements.push("): TransactionInstruction {".to_owned());
-    statements.push("const data = Buffer.from(this.serialize());".to_owned());
-    statements.push("let keys: AccountKey[] = [];".to_owned());
-    statements.extend(keys_statements);
-    statements.push("return new TransactionInstruction({".to_owned());
-    statements.push("keys,".to_owned());
-    statements.push("programId,".to_owned());
-    statements.push("data,".to_owned());
-    statements.push("});".to_owned());
-    statements.push("}".to_owned());
-    statements.push("}".to_owned());
-    let mut out_string = String::new();
-    for s in statements {
-        out_string.push_str(&s);
-        out_string.push('\n');
-    }
-    out_string
-}
-
-fn type_assignment(ty: &Type, camel_case_ident: &str) -> String {
-    match ty {
-        Type::Path(_) => {
-            let simple_type = get_simple_type(ty);
-            match simple_type.as_ref() {
-                "i8" | "i16" | "i32" => {
-                    let bit_width = simple_type[1..].parse::<u8>().unwrap();
-                    format!(
-                        "this.{} = new BN(obj.{}).fromTwos({}).toNumber();",
-                        camel_case_ident, camel_case_ident, bit_width
-                    )
-                }
-                "i64" | "i128" => {
-                    let bit_width = simple_type[1..].parse::<u8>().unwrap();
-                    format!(
-                        "this.{} = obj.{}.fromTwos({});",
-                        camel_case_ident, camel_case_ident, bit_width
-                    )
-                }
-                _ => format!("this.{} = obj.{};", camel_case_ident, camel_case_ident),
-            }
-        }
-        Type::Array(TypeArray {
-            bracket_token: _,
-            elem,
-            semi_token: _,
-            len:
-                Expr::Lit(ExprLit {
-                    attrs: _,
-                    lit: Lit::Int(_),
-                }),
-        }) => {
-            let simple_type = get_simple_type(elem);
-            match &simple_type as &str {
-                "i8" | "i16" | "i32" => {
-                    let bit_width = simple_type[1..].parse::<u8>().unwrap();
-                    format!(
-                        "this.{} = obj.{}.map(o => new BN(o).fromTwos({}).toNumber());",
-                        camel_case_ident, camel_case_ident, bit_width
-                    )
-                }
-                "i64" | "i128" => {
-                    let bit_width = simple_type[1..].parse::<u8>().unwrap();
-                    format!(
-                        "this.{} = obj.{}.map(o => o.fromTwos({}));",
-                        camel_case_ident, camel_case_ident, bit_width
-                    )
-                }
-                _ => format!("this.{} = obj.{};", camel_case_ident, camel_case_ident),
-            }
-        }
-        _ => unimplemented!(),
+pub fn get_header(target_lang: TargetLang) -> String {
+    match target_lang {
+        TargetLang::Javascript => include_str!("templates/template.ts").to_string(),
+        TargetLang::Python => include_str!("templates/template.py").to_string(),
     }
 }
 
@@ -295,141 +110,6 @@ fn get_simple_type(ty: &Type) -> String {
                 segments,
             },
         }) => segments.iter().next().unwrap().ident.to_string(),
-        _ => unimplemented!(),
-    }
-}
-
-fn type_to_js(ty: &Type) -> String {
-    match ty {
-        Type::Path(TypePath {
-            qself: _,
-            path: Path {
-                leading_colon: _,
-                segments,
-            },
-        }) => {
-            let segment = segments.iter().next().unwrap();
-            let simple_type = segment.ident.to_string();
-            match simple_type.as_ref() {
-                "u8" | "u16" | "u32" | "i8" | "i16" | "i32" => "number".to_owned(),
-                "u64" | "u128" | "i64" | "i128" => "BN".to_owned(),
-                "String" => "string".to_owned(),
-                "Pubkey" => "Uint8Array".to_owned(),
-                "Vec" => {
-                    if let PathArguments::AngleBracketed(AngleBracketedGenericArguments {
-                        colon2_token: _,
-                        lt_token: _,
-                        args,
-                        gt_token: _,
-                    }) = &segment.arguments
-                    {
-                        if let GenericArgument::Type(t) = args.first().unwrap() {
-                            let inner_type = type_to_js(t);
-                            return format!("{}[]", &inner_type);
-                        } else {
-                            unimplemented!()
-                        }
-                    } else {
-                        unreachable!()
-                    };
-                }
-                _ => "number".to_owned(), // We assume this is an enum
-            }
-        }
-        Type::Array(TypeArray {
-            bracket_token: _,
-            elem,
-            semi_token: _,
-            len: _,
-        }) => {
-            let inner_type = type_to_borsh(elem);
-            array_to_js(&inner_type)
-        }
-        _ => unimplemented!(),
-    }
-}
-
-fn array_to_js(inner_type: &str) -> String {
-    match inner_type as &str {
-        "\"u8\"" | "\"i8\"" => "Uint8Array",
-        "\"u16\"" | "\"i16\"" | "\"u32\"" | "\"i32\"" => "number[]",
-        "\"u64\"" | "\"i64\"" | "\"u128\"" | "\"i128\"" => "BN[]",
-        _ => unimplemented!(),
-    }
-    .to_owned()
-}
-
-fn type_to_borsh(ty: &Type) -> String {
-    match ty {
-        Type::Path(TypePath {
-            qself: _,
-            path: Path {
-                leading_colon: _,
-                segments,
-            },
-        }) => {
-            let segment = segments.iter().next().unwrap();
-            let simple_type = segment.ident.to_string();
-            let t = match simple_type.as_ref() {
-                "u8" | "u16" | "u32" | "u64" | "u128" => simple_type,
-                "i8" | "i16" | "i32" | "i64" | "i128" => {
-                    let mut res = "u".to_owned();
-                    <String as std::fmt::Write>::write_str(&mut res, &simple_type[1..]).unwrap();
-                    res
-                }
-                "String" => "string".to_owned(),
-                "Pubkey" => return "[32]".to_owned(),
-                "Vec" => {
-                    if let PathArguments::AngleBracketed(AngleBracketedGenericArguments {
-                        colon2_token: _,
-                        lt_token: _,
-                        args,
-                        gt_token: _,
-                    }) = &segment.arguments
-                    {
-                        if let GenericArgument::Type(t) = args.first().unwrap() {
-                            let inner_type = type_to_borsh(t);
-                            return format!("[{}]", &inner_type);
-                        } else {
-                            unimplemented!()
-                        }
-                    } else {
-                        unreachable!()
-                    };
-                }
-                _ => "u8".to_owned(), // We assume this is an enum
-            };
-            format!("\"{}\"", t)
-        }
-        Type::Array(TypeArray {
-            bracket_token: _,
-            elem,
-            semi_token: _,
-            len:
-                Expr::Lit(ExprLit {
-                    attrs: _,
-                    lit: Lit::Int(l),
-                }),
-        }) => {
-            let inner_type = type_to_borsh(elem);
-            let mut unsigned_type = "u".to_owned();
-            <String as std::fmt::Write>::write_str(
-                &mut unsigned_type,
-                &inner_type[2..inner_type.len() - 1],
-            )
-            .unwrap();
-
-            match &unsigned_type as &str {
-                "u8" => format!("[{}]", l.base10_parse::<u8>().unwrap()),
-                "u16" | "u32" | "u64" | "u128" => {
-                    format!("[{}, {}]", inner_type, l.base10_parse::<u8>().unwrap())
-                }
-                _ => {
-                    println!("{:?}", inner_type);
-                    unimplemented!()
-                }
-            }
-        }
         _ => unimplemented!(),
     }
 }
@@ -470,8 +150,14 @@ fn padding_len(ty: &Type) -> u8 {
 fn snake_to_camel(s: &str) -> String {
     s.from_case(Case::Snake).to_case(Case::Camel)
 }
+fn snake_to_pascal(s: &str) -> String {
+    s.from_case(Case::Snake).to_case(Case::Pascal)
+}
 fn pascal_to_snake(s: &str) -> String {
     s.from_case(Case::Pascal).to_case(Case::Snake)
+}
+fn lower_to_upper(s: &str) -> String {
+    s.from_case(Case::Lower).to_case(Case::Upper)
 }
 
 fn find_struct(ident_str: &str, file_ast: &syn::File) -> Item {
--- autobindings/src/main.rs
+++ autobindings/src/main.rs
@@ -1,4 +1,4 @@
-use cargo_autobindings::generate;
+use cargo_autobindings::{generate, TargetLang};
 use clap::{App, Arg};
 
 fn main() {
@@ -7,21 +7,43 @@ fn main() {
         .author("Bonfida")
         .arg(Arg::with_name("name").hidden(true).required(true))
         .arg(
-            Arg::with_name("instructions_path")
+            Arg::with_name("instr-path")
+                .long("instructions-path")
                 .takes_value(true)
                 .default_value("src/processor"),
         )
         .arg(
-            Arg::with_name("instructions_enum_path")
+            Arg::with_name("instr-enum-path")
+                .long("instructions-enum-path")
                 .takes_value(true)
                 .default_value("src/instruction.rs"),
         )
+        .arg(
+            Arg::with_name("target-lang")
+                .long("target-language")
+                .takes_value(true)
+                .default_value("js")
+                .help("Enter \"py\" or \"js\""),
+        )
         .get_matches();
-    let instructions_path = matches.value_of("instructions_path").unwrap();
-    let instructions_enum_path = matches.value_of("instructions_enum_path").unwrap();
+    let instructions_path = matches.value_of("instr-path").unwrap();
+    let instructions_enum_path = matches.value_of("instr-enum-path").unwrap();
+    let target_lang_str = matches.value_of("target-lang").unwrap();
+    let target_lang = match target_lang_str {
+        "js" | "javascript" => TargetLang::Javascript,
+        "py" | "python" => TargetLang::Python,
+        _ => {
+            println!("Target language must be javascript or python");
+            panic!()
+        }
+    };
     generate(
         instructions_path,
         instructions_enum_path,
-        "../js/src/raw_instructions.ts",
+        target_lang,
+        match target_lang {
+            TargetLang::Javascript => "../js/src/raw_instructions.ts",
+            TargetLang::Python => "../python/src/raw_instructions.py",
+        },
     );
 }
