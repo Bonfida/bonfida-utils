89e54886726c7e7df9bd748e2ae064ff779226a7
ellttBen <elliott@bonfida.com>
2022-07-26 16:51:09 +0100

    Add warp_to_timestamp and sign_send_instructions methods

--- test-utils/src/lib.rs
+++ test-utils/src/lib.rs
@@ -1,4 +1,3 @@
 pub mod error;
 pub mod program_test_context_ext;
 pub mod program_test_ext;
-pub mod utils;
--- test-utils/src/program_test_context_ext.rs
+++ test-utils/src/program_test_context_ext.rs
@@ -1,9 +1,11 @@
 use async_trait::async_trait;
-use solana_program::{program_pack::Pack, pubkey::Pubkey};
+use solana_program::{clock::Clock, instruction::Instruction, program_pack::Pack, pubkey::Pubkey};
 use solana_program_test::ProgramTestContext;
-use solana_sdk::{signature::Keypair, signer::Signer};
+use solana_sdk::{signature::Keypair, signer::Signer, transaction::Transaction};
 
-use crate::{error::TestError, utils::sign_send_instructions};
+use crate::error::TestError;
+
+const NANOSECONDS_IN_SECOND: u128 = 1_000_000_000;
 
 #[async_trait]
 pub trait ProgramTestContextExt {
@@ -19,6 +21,14 @@ pub trait ProgramTestContextExt {
         &mut self,
         key: Pubkey,
     ) -> Result<spl_token::state::Account, TestError>;
+
+    async fn sign_send_instructions(
+        &mut self,
+        instructions: &[Instruction],
+        signers: &[&Keypair],
+    ) -> Result<(), TestError>;
+
+    async fn warp_to_timestamp(&mut self, timestamp: i64) -> Result<(), TestError>;
 }
 
 #[async_trait]
@@ -38,7 +48,8 @@ impl ProgramTestContextExt for ProgramTestContext {
             &[],
             amount,
         )?;
-        sign_send_instructions(self, vec![mint_instruction], &[mint_authority]).await?;
+        self.sign_send_instructions(&[mint_instruction], &[mint_authority])
+            .await?;
         Ok(())
     }
     async fn get_token_account(
@@ -54,5 +65,36 @@ impl ProgramTestContextExt for ProgramTestContext {
             return Err(TestError::InvalidTokenAccount);
         }
         Ok(spl_token::state::Account::unpack(&raw_account.data)?)
+    }
+    async fn sign_send_instructions(
+        &mut self,
+        instructions: &[Instruction],
+        signers: &[&Keypair],
+    ) -> Result<(), TestError> {
+        let mut transaction = Transaction::new_with_payer(instructions, Some(&self.payer.pubkey()));
+        let mut payer_signers = Vec::with_capacity(1 + signers.len());
+        payer_signers.push(&self.payer);
+        for s in signers {
+            payer_signers.push(s);
+        }
+        transaction.partial_sign(&payer_signers, self.last_blockhash);
+        self.banks_client.process_transaction(transaction).await?;
+        Ok(())
+    }
+
+    async fn warp_to_timestamp(&mut self, timestamp: i64) -> Result<(), TestError> {
+        let mut clock = self.banks_client.get_sysvar::<Clock>().await?;
+        if clock.unix_timestamp > timestamp {
+            return Err(TestError::InvalidTimestampForWarp);
+        }
+        let ns_per_slot = self.genesis_config().ns_per_slot();
+        let time_delta_ns =
+            (timestamp - clock.unix_timestamp).unsigned_abs() as u128 * NANOSECONDS_IN_SECOND;
+        let number_of_slots_to_warp: u64 = (time_delta_ns / ns_per_slot).try_into().unwrap();
+
+        clock.unix_timestamp = timestamp;
+        self.set_sysvar(&clock);
+        self.warp_to_slot(clock.slot + number_of_slots_to_warp)?;
+        Ok(())
     }
 }
