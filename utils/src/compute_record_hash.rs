343a258cd6d7b5ddd715399c2602ac298516fd90
ellttBen <elliott@bonfida.com>
2022-11-04 12:55:04 +0000

    Add compute_record_hash macro to bonfida-utils

---
+++ utils/src/compute_record_hash.rs
@@ -0,0 +1,14 @@
+#[cfg(test)]
+mod tests {
+
+    use solana_program::hash::hashv;
+
+    use bonfida_macros::compute_hashv;
+
+    #[test]
+    pub fn functional() {
+        let record_hash = compute_hashv!("ETH");
+        let record_hash_ref = hashv(&["SPL Name Service\0ETH".as_bytes()]).to_bytes();
+        assert_eq!(record_hash, record_hash_ref);
+    }
+}
--- utils/src/lib.rs
+++ utils/src/lib.rs
@@ -6,6 +6,7 @@ declare_id!("11111111111111111111111111111111");
 mod accounts;
 mod borsh_size;
 pub mod checks;
+mod compute_record_hash;
 mod declare_id_with_central_state;
 mod wrapped_pod;
 
@@ -13,7 +14,8 @@ pub mod fp_math;
 pub mod pyth;
 pub use accounts::InstructionsAccount;
 pub use bonfida_macros::{
-    declare_id_with_central_state, BorshSize, InstructionsAccount, WrappedPod, WrappedPodMut,
+    compute_hashv as compute_record_hash, declare_id_with_central_state, BorshSize,
+    InstructionsAccount, WrappedPod, WrappedPodMut,
 };
 pub use borsh_size::BorshSize;
 pub use wrapped_pod::{WrappedPod, WrappedPodMut};
