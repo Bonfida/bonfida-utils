d972ebe34565557bb3377920a80d1b46d19c9f1b
ellttben <elliott.benisty@gmail.com>
2024-10-17 10:47:01 +0100

    Add support for slice fields in WrappedPod and WrappedPodMut at any position. Backwards compatible.

--- crates/macros/src/wrapped_pod.rs
+++ crates/macros/src/wrapped_pod.rs
@@ -1,4 +1,4 @@
-use proc_macro2::TokenStream;
+use proc_macro2::{Ident, TokenStream};
 use quote::quote;
 use syn::{Type, TypeSlice};
 
@@ -13,20 +13,24 @@ pub fn process(mut ast: syn::DeriveInput, is_mut: bool) -> TokenStream {
                 }),
             ..
         }) => {
-            let mut lengths = Vec::with_capacity(named.len());
-            let mut field_idents = Vec::with_capacity(named.len());
-            let mut cast_to_bytes_statements = Vec::with_capacity(named.len());
-            let mut cast_from_bytes_statements = Vec::with_capacity(named.len());
-            let mut split_statements = Vec::with_capacity(named.len());
+            let number_of_fields = named.len();
+            let mut lengths = Vec::with_capacity(number_of_fields);
+            let mut field_idents = Vec::with_capacity(number_of_fields);
+            let mut cast_to_bytes_statements = Vec::with_capacity(number_of_fields);
+            let mut cast_from_bytes_statements = Vec::with_capacity(number_of_fields);
+            let mut split_statements = Vec::with_capacity(number_of_fields);
+            let split_ident = if is_mut {
+                Ident::new("split_at_mut", struct_ident.span())
+            } else {
+                Ident::new("split_at", struct_ident.span())
+            };
 
-            for n in named.into_iter() {
+            for (i, n) in named.into_iter().enumerate() {
+                let is_last = i + 1 == number_of_fields;
                 let ident = n.ident.clone().unwrap();
                 if let Type::Reference(t) = n.ty.clone() {
                     match *t.elem {
                         Type::Slice(TypeSlice { elem, .. }) => {
-                            lengths.push(quote!(self.#ident.len() * std::mem::size_of::<#elem>()));
-                            cast_to_bytes_statements
-                                .push(quote!(bytemuck::cast_slice::<_, u8>(self.#ident)));
                             if is_mut {
                                 cast_from_bytes_statements
                                     .push(quote!(bytemuck::cast_slice_mut::<u8, _>(#ident)));
@@ -34,13 +38,32 @@ pub fn process(mut ast: syn::DeriveInput, is_mut: bool) -> TokenStream {
                                 cast_from_bytes_statements
                                     .push(quote!(bytemuck::cast_slice::<u8, _>(#ident)));
                             }
-                            split_statements.push(quote!(let #ident = buffer;));
+                            if is_last {
+                                cast_to_bytes_statements
+                                    .push(quote!(bytemuck::cast_slice::<_, u8>(self.#ident)));
+                                split_statements.push(quote!(let #ident = buffer;));
+
+                                lengths
+                                    .push(quote!(self.#ident.len() * std::mem::size_of::<#elem>()));
+                            } else {
+                                let len_ident = Ident::new(&format!("{ident}_len"), ident.span());
+                                split_statements.push(quote! {
+                                    let (#len_ident, buffer) = buffer.#split_ident(8);
+                                    let #len_ident: &u64 = bytemuck::from_bytes(#len_ident);
+                                    let (#ident, buffer) = buffer.#split_ident(*#len_ident as usize);
+                                });
+                                cast_to_bytes_statements
+                                    .push(quote!(((self.#ident.len() * std::mem::size_of::<#elem>()) as u64).to_le_bytes()));
+                                cast_to_bytes_statements
+                                    .push(quote!(bytemuck::cast_slice::<_, u8>(self.#ident)));
+                                lengths.push(
+                                    quote!(self.#ident.len() * std::mem::size_of::<#elem>() + 8),
+                                );
+                            }
                         }
                         Type::Path(p)
                             if p.path.get_ident().map(|s| s == "str").unwrap_or(false) =>
                         {
-                            lengths.push(quote!(self.#ident.len()));
-                            cast_to_bytes_statements.push(quote!(self.#ident.as_bytes()));
                             if is_mut {
                                 cast_from_bytes_statements
                                     .push(quote!(std::str::from_utf8_mut(#ident).unwrap()));
@@ -48,7 +71,23 @@ pub fn process(mut ast: syn::DeriveInput, is_mut: bool) -> TokenStream {
                                 cast_from_bytes_statements
                                     .push(quote!(std::str::from_utf8(#ident).unwrap()));
                             }
-                            split_statements.push(quote!(let #ident = buffer;));
+                            if is_last {
+                                cast_to_bytes_statements.push(quote!(self.#ident.as_bytes()));
+                                split_statements.push(quote!(let #ident = buffer;));
+
+                                lengths.push(quote!(self.#ident.len()));
+                            } else {
+                                let len_ident = Ident::new(&format!("{ident}_len"), ident.span());
+                                split_statements.push(quote! {
+                                    let (#len_ident, buffer) = buffer.#split_ident(8);
+                                    let #len_ident: &u64 = bytemuck::from_bytes(#len_ident);
+                                    let (#ident, buffer) = buffer.#split_ident(*#len_ident as usize);
+                                });
+                                cast_to_bytes_statements
+                                    .push(quote!((self.#ident.len() as u64).to_le_bytes()));
+                                cast_to_bytes_statements.push(quote!(self.#ident.as_bytes()));
+                                lengths.push(quote!(self.#ident.len() + 8));
+                            }
                         }
                         Type::Path(p) => {
                             let len = quote!(std::mem::size_of::<#p>());
--- crates/utils/src/wrapped_pod.rs
+++ crates/utils/src/wrapped_pod.rs
@@ -19,20 +19,37 @@ pub mod tests {
     #[derive(WrappedPodMut, PartialEq, Debug)]
     pub struct TestStructMut<'a> {
         pub a: &'a mut u64,
-        pub b: &'a mut [u128],
+        pub b: &'a mut [u32],
+        pub c: &'a mut [u128],
     }
 
     #[derive(WrappedPod, PartialEq, Debug)]
     pub struct TestStruct<'a> {
         pub a: &'a u64,
-        pub b: &'a [u128],
+        pub b: &'a [u32],
+        pub c: &'a [u128],
+    }
+
+    #[derive(WrappedPodMut, PartialEq, Debug)]
+    pub struct TestStructMut2<'a> {
+        pub a: &'a mut u64,
+        pub b: &'a mut str,
+        pub c: &'a mut str,
+    }
+
+    #[derive(WrappedPod, PartialEq, Debug)]
+    pub struct TestStruct2<'a> {
+        pub a: &'a u64,
+        pub b: &'a str,
+        pub c: &'a str,
     }
 
     #[test]
     pub fn test_mut() {
-        let a = &mut 42;
-        let b = &mut [198, 2987239847, 234820357, 45];
-        let o = TestStructMut { a, b };
+        let a = &mut rand::random();
+        let b = &mut rand::random::<[u32; 4]>();
+        let c = &mut rand::random::<[u128; 7]>();
+        let o = TestStructMut { a, b, c };
         let mut buf = Vec::with_capacity(o.size());
         o.export(&mut buf);
         assert_eq!(buf.len(), o.size());
@@ -42,13 +59,40 @@ pub mod tests {
 
     #[test]
     pub fn test() {
-        let a = &mut 42;
-        let b = &mut [198, 2987239847, 234820357, 45];
-        let o = TestStruct { a, b };
+        let a = &rand::random();
+        let b = &rand::random::<[u32; 4]>();
+        let c = &rand::random::<[u128; 7]>();
+        let o = TestStruct { a, b, c };
         let mut buf = Vec::with_capacity(o.size());
         o.export(&mut buf);
         assert_eq!(buf.len(), o.size());
         let o2 = TestStruct::from_bytes(&buf);
+        assert_eq!(o2, o)
+    }
+
+    #[test]
+    pub fn test_mut_2() {
+        let a = &mut rand::random();
+        let b = &mut (0..10).map(|_| rand::random::<char>()).collect::<String>();
+        let c = &mut (0..15).map(|_| rand::random::<char>()).collect::<String>();
+        let o = TestStructMut2 { a, b, c };
+        let mut buf = Vec::with_capacity(o.size());
+        o.export(&mut buf);
+        assert_eq!(buf.len(), o.size());
+        let o2 = TestStructMut2::from_bytes(&mut buf);
+        assert_eq!(o2, o)
+    }
+
+    #[test]
+    pub fn test_2() {
+        let a = &rand::random();
+        let b = &(0..10).map(|_| rand::random::<char>()).collect::<String>();
+        let c = &(0..15).map(|_| rand::random::<char>()).collect::<String>();
+        let o = TestStruct2 { a, b, c };
+        let mut buf = Vec::with_capacity(o.size());
+        o.export(&mut buf);
+        assert_eq!(buf.len(), o.size());
+        let o2 = TestStruct2::from_bytes(&buf);
         assert_eq!(o2, o)
     }
 }
