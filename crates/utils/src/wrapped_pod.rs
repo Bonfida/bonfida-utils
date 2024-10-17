1bf56d71bcd78ce95a1135f46d40ec279d4a64a5
ellttben <elliott.benisty@gmail.com>
2024-10-17 13:42:15 +0100

    Add comparative testing with macros 0.6.2

--- crates/utils/src/wrapped_pod.rs
+++ crates/utils/src/wrapped_pod.rs
@@ -44,6 +44,30 @@ pub mod tests {
         pub c: &'a str,
     }
 
+    #[derive(bonfida_macros_old::WrappedPodMut, PartialEq, Debug)]
+    pub struct CompatTestStructMutOld<'a> {
+        pub a: &'a mut u64,
+        pub b: &'a mut [u32],
+    }
+
+    #[derive(bonfida_macros_old::WrappedPod, PartialEq, Debug)]
+    pub struct CompatTestStructOld<'a> {
+        pub a: &'a u64,
+        pub b: &'a [u32],
+    }
+
+    #[derive(WrappedPodMut, PartialEq, Debug)]
+    pub struct CompatTestStructMutNew<'a> {
+        pub a: &'a mut u64,
+        pub b: &'a mut [u32],
+    }
+
+    #[derive(WrappedPod, PartialEq, Debug)]
+    pub struct CompatTestStructNew<'a> {
+        pub a: &'a u64,
+        pub b: &'a [u32],
+    }
+
     #[test]
     pub fn test_mut() {
         let a = &mut rand::random();
@@ -105,5 +129,44 @@ pub mod tests {
         assert_eq!(buf.len(), size_of::<u64>() + 8 + o.b.len() + o.c.len());
         let o2 = TestStruct2::from_bytes(&buf);
         assert_eq!(o2, o)
+    }
+
+    #[test]
+    pub fn test_back_compat() {
+        let a = &rand::random();
+        let b = &rand::random::<[u32; 4]>();
+        let o_old_reference = CompatTestStructOld { a, b };
+        let o_new_reference = CompatTestStructNew { a, b };
+        let mut buf_new = Vec::with_capacity(o_new_reference.size());
+        o_new_reference.export(&mut buf_new);
+        let o_old = CompatTestStructOld::from_bytes(&buf_new);
+        assert_eq!(o_old, o_old_reference);
+
+        let mut buf_old = Vec::with_capacity(o_old.size());
+        o_old_reference.export(&mut buf_old);
+        let o_new = CompatTestStructNew::from_bytes(&buf_old);
+        assert_eq!(o_new, o_new_reference);
+    }
+
+    #[test]
+    pub fn test_back_compat_mut() {
+        let a: &mut u64 = &mut rand::random();
+        let b = &mut rand::random::<[u32; 4]>();
+        let a_clone = &mut a.clone();
+        let b_clone = &mut b.clone();
+        let o_old_reference = CompatTestStructMutOld {
+            a: a_clone,
+            b: b_clone,
+        };
+        let o_new_reference = CompatTestStructMutNew { a, b };
+        let mut buf_new = Vec::with_capacity(o_new_reference.size());
+        o_new_reference.export(&mut buf_new);
+        let o_old = CompatTestStructMutOld::from_bytes(&mut buf_new);
+        assert_eq!(o_old, o_old_reference);
+
+        let mut buf_old = Vec::with_capacity(o_old.size());
+        o_old_reference.export(&mut buf_old);
+        let o_new = CompatTestStructMutNew::from_bytes(&mut buf_old);
+        assert_eq!(o_new, o_new_reference);
     }
 }
