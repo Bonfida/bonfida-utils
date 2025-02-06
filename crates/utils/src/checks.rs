7ed8c5dbf42533786bd9da1666b71567ae9d41d8
DR497 <47689875+dr497@users.noreply.github.com>
2025-02-06 10:48:58 +0800

    utils: track caller for check functions

--- crates/utils/src/checks.rs
+++ crates/utils/src/checks.rs
@@ -8,32 +8,57 @@ use solana_program::{
     sysvar::{rent::Rent, Sysvar},
 };
 use spl_token::state::Account;
+use std::panic::Location;
 
 // Safety verification functions
+#[track_caller]
 pub fn check_account_key(account: &AccountInfo, key: &Pubkey) -> ProgramResult {
     if account.key != key {
-        msg!("Wrong account key: {} should be {}", account.key, key);
+        let caller = Location::caller();
+        msg!(
+            "Wrong account key: {} should be {} - File: {} - Line: {}",
+            account.key,
+            key,
+            caller.file(),
+            caller.line()
+        );
         return Err(ProgramError::InvalidArgument);
     }
     Ok(())
 }
 
+#[track_caller]
 pub fn check_account_owner(account: &AccountInfo, owner: &Pubkey) -> ProgramResult {
     if account.owner != owner {
-        msg!("Wrong account owner: {} should be {}", account.owner, owner);
+        let caller = Location::caller();
+        msg!(
+            "Wrong account owner: {} should be {} - File: {} - Line: {}",
+            account.owner,
+            owner,
+            caller.file(),
+            caller.line()
+        );
         return Err(ProgramError::InvalidArgument);
     }
     Ok(())
 }
 
+#[track_caller]
 pub fn check_signer(account: &AccountInfo) -> ProgramResult {
     if !(account.is_signer) {
-        msg!("Missing signature for: {}", account.key);
+        let caller = Location::caller();
+        msg!(
+            "Missing signature for: {} - File: {} - Line: {}",
+            account.key,
+            caller.file(),
+            caller.line()
+        );
         return Err(ProgramError::MissingRequiredSignature);
     }
     Ok(())
 }
 
+#[track_caller]
 pub fn check_account_derivation(
     account: &AccountInfo,
     seeds: &[&[u8]],
@@ -52,6 +77,7 @@ pub fn check_rent_exempt(account: &AccountInfo) -> ProgramResult {
     Ok(())
 }
 
+#[track_caller]
 pub fn check_token_account_owner(
     account: &AccountInfo,
     owner: &Pubkey,
@@ -59,10 +85,13 @@ pub fn check_token_account_owner(
     check_account_owner(account, &spl_token::ID)?;
     let token_account = Account::unpack_from_slice(&account.data.borrow())?;
     if token_account.owner != *owner {
+        let caller = Location::caller();
         msg!(
-            "Wrong account owner: {} should be {}",
+            "Wrong account owner: {} should be {} - File: {} - Line: {}",
             token_account.owner,
-            owner
+            owner,
+            caller.file(),
+            caller.line()
         );
         return Err(ProgramError::InvalidArgument);
     }
